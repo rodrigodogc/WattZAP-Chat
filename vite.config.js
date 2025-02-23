efe53a1356ed1249e93ff4ac6c512879b9dcb332
Rodrigo Silva <rodrigodogc2013@gmail.com>
2025-02-23 01:32:54 -0300

    Deploy github setado

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/WattZAP-Chat",
   server: {
     host: '0.0.0.0'
-  }
+  },
 })
